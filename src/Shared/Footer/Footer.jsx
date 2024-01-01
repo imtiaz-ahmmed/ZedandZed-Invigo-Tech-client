020819f90026dfc2014126432405e8d52f57d55b
imtiaz-ahmmed <imtiazahmmed00@gmail.com>
2024-01-01 16:10:34 +0600

    Footer modified

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer font-bold footer-center p-4 bg-base-300 text-base-content mt-56 md:mt-96">
+    <footer className="footer font-bold footer-center p-4 bg-black text-white text-base-content mt-56 md:mt-96">
       <aside>
         Copyright &copy; {new Date().getFullYear()} || Zedandzed IT Services||
         All rights reserved.
